bbfe122ed729e935bd59dd1581853ae5234c454f
hawik <hawiksyaef@gmail.com>
2022-08-20 19:45:01 +0700

    menambahkan table di userlist

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -11,11 +11,34 @@ const UserList = () => {
                         <input 
                             type="text" 
                             className="input" 
-                            placeholder='Find something here ...' />
+                            placeholder='Find something here ...' 
+                        />
+                        
                     </div>
-                    <div className="control"></div>
+                    <div className="control">
+                        <button type='submit' className='button is-info'>Search</button>
+                    </div>
                 </div>
             </form>
+            
+            {/* table>(theadtr>th*4)+(tbody>tr>td*4) */}
+            <table className='table is-striped is-bordered is-fullwidth mt-2'>
+                <theadtr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Gender</th>
+                </theadtr>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+
             </div>
         </div>
     </div>
